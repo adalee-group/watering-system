42aff540c5d39d239f9de5de4f17b8cc781a310a
陈博洋 <kuzoncby@gmail.com>
2016-09-06 22:55:28 +0800

    Add a rest template on android, not working yet.

--- android/app/src/main/java/edu/hucare/watering/LoginActivity.java
+++ android/app/src/main/java/edu/hucare/watering/LoginActivity.java
@@ -2,11 +2,18 @@ package edu.hucare.watering;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.EditText;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import edu.hucare.model.User;
 
 /**
  * A login screen that offers login via email/password.
@@ -24,8 +31,64 @@ public class LoginActivity extends Activity {
 
     @Click
     void btnSignIn() {
-        Intent intent = new Intent(this, DeviceActivity_.class);
-        startActivity(intent);
+
+        try {
+            User origin = new User();
+            origin.setEmail(etEmail.getText().toString());
+            origin.setPassword(etPassword.getText().toString());
+            new DoLogin().execute(origin);
+            Intent intent = new Intent(this, DeviceActivity_.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public User login() {
+        final String url = "http://192.168.10.108:1984/user/";
+        User origin = new User();
+        origin.setEmail(etEmail.getText().toString());
+        origin.setPassword(etPassword.getText().toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+        User user = restTemplate.postForObject(url, origin, User.class);
+
+        return user;
+    }
+
+    class DoLogin extends AsyncTask<User, User, User> {
+
+
+        protected User doInBackground(User... users) {
+
+            try {
+
+                final String url = "http://192.168.10.108:1984/user/";
+
+
+                RestTemplate restTemplate = new RestTemplate();
+
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+                User user = restTemplate.postForObject(url, users[0], User.class);
+                return user;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        protected void onPostExecute(String file_url) {
+        }
     }
 
 }
--- android/app/src/main/java/edu/hucare/watering/ScheduleActivity.java
+++ android/app/src/main/java/edu/hucare/watering/ScheduleActivity.java
@@ -13,16 +13,16 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
